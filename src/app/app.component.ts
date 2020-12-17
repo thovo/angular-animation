230a2fe76320c46046e0e35568ffb7b3f403331a
Tho Vo <votuongtho@gmail.com>
2020-12-17 19:21:02 +0100

    Add some small adjustment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-animation';
+  animationSpeed: number = 1000;
+  @ViewChild('box') box: ElementRef;
+
+  handleMouseClick(event: MouseEvent) {
+    const x = event.clientX;
+    const y = event.clientY;
+    this.box.nativeElement.style.left = x - 30 + 'px';
+    this.box.nativeElement.style.top = y - 30 + 'px';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
